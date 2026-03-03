4989707cba39f7ac7fb745590c8ae95c45c2b1a0
Parkhunsang <daxi99@naver.com>
2026-03-03 16:09:48 +0900

    인기투표-검색,필터 기능 추가

--- animal/src/main/java/com/example/animal/dto/VillagerList.java
+++ animal/src/main/java/com/example/animal/dto/VillagerList.java
@@ -9,4 +9,6 @@ public class VillagerList {
     private String villagerImageIcon; // villager_image_icon
     private String villagerImage; // villager_image
     private String villagerBirth; // MM-DD
+
+    private int memberNo; // 투표한 회원 번호
 }
