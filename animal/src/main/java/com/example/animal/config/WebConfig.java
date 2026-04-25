51dfc13cec67f923d7d1d1b484ca840992250560
seharuem <seharuem@naver.com>
2026-04-25 23:48:23 +0900

    사파리 로그인 테스트

--- animal/src/main/java/com/example/animal/config/WebConfig.java
+++ animal/src/main/java/com/example/animal/config/WebConfig.java
@@ -4,6 +4,9 @@ package com.example.animal.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -13,5 +16,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:5173", "https://animalcrossingrank.netlify.app", "https://animal-guide.pages.dev")
                 .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                 .allowCredentials(true);
+    }
+
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setSameSite("None");
+        serializer.setUseSecureCookie(true);
+        serializer.setCookiePath("/");
+        return serializer;
     }
 }
